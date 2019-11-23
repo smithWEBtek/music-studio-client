8ce1992e615d99ba3e64d6dfabde22187b33b90c
smithwebtek <brad@smithwebtek.com>
2019-11-23 12:53:47 -0500

    Updates

---
+++ precache-manifest.19c201f009adfeb73418c4319ff51e01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3e4d27e91648005b23d1259bc3df1e",
+    "url": "/smithWEBtek/music-studio2/index.html"
+  },
+  {
+    "revision": "5d9e0c588d3cf1f7a891",
+    "url": "/smithWEBtek/music-studio2/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "61cadf6bfdd5d394e0a2",
+    "url": "/smithWEBtek/music-studio2/static/css/main.a0dba1c5.chunk.css"
+  },
+  {
+    "revision": "5d9e0c588d3cf1f7a891",
+    "url": "/smithWEBtek/music-studio2/static/js/2.4bc647ae.chunk.js"
+  },
+  {
+    "revision": "61cadf6bfdd5d394e0a2",
+    "url": "/smithWEBtek/music-studio2/static/js/main.b491da9a.chunk.js"
+  },
+  {
+    "revision": "0b67966c911b6974cae7",
+    "url": "/smithWEBtek/music-studio2/static/js/runtime-main.599484ac.js"
+  },
+  {
+    "revision": "9fbd4e27336875cf82d77e5a3775783c",
+    "url": "/smithWEBtek/music-studio2/static/media/LogoSpin.9fbd4e27.png"
+  }
+]);
